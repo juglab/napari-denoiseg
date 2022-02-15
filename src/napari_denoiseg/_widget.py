62060bb68b8e447ef5e3319431cf9742d5b92e76
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-02-15 17:23:51 +0100

    add split train/val

--- src/napari_denoiseg/_widget.py
+++ src/napari_denoiseg/_widget.py
@@ -11,6 +11,8 @@ from magicgui import magic_factory, widgets
 import time
 
 
+# "step 50/200 25%"
+
 @magic_factory(perc_train_labels={"widget_type": "FloatSlider", "min": 0.1, "max": 1., "step": 0.05, 'value': 0.6},
                n_epochs={"widget_type": "SpinBox", "step": 1, 'value': 10},
                n_steps={"widget_type": "SpinBox", "step": 1, 'value': 200},
@@ -58,6 +60,9 @@ def denoiseg_widget(data: 'napari.layers.Image',
     n_labels = int(0.5 + perc_train_labels * ground_truth.data.shape[0])
     print(f'Number of labels used for training {n_labels}')
 
+    # split train and val
+    X, Y, X_val, Y_val = __prepare_data(data.data, ground_truth.data, perc_train_labels)
+
     config = {'n_epochs': n_epochs,
               'n_steps': n_steps,
               'patch_shape': patch_shape,
@@ -68,8 +73,53 @@ def denoiseg_widget(data: 'napari.layers.Image',
     process()
 
 
-def __prepare_data():
-    pass
+def __prepare_data(data, gt, perc_labels):
+    import numpy as np
+    from denoiseg.utils.misc_utils import augment_data
+    from denoiseg.utils.seg_utils import convert_to_oneHot
+
+    def zero_sum(im):
+        im_reshaped = im.reshape(im.shape[0], -1)
+
+        return np.where(np.sum(im_reshaped != 0, axis=1) != 0)[0]
+
+    def list_diff(l1, l2):
+        return list(set(l1) - set(l2))
+
+    def create_train_set(x, y, ind_exclude):
+        masks = np.zeros(x.shape)
+        masks[0:y.shape[0], 0:y.shape[1], 0:y.shape[2]] = y  # there's probably a more elegant way
+
+        return augment_data(np.delete(x, ind_exclude, axis=0), np.delete(masks, ind_exclude, axis=0))
+
+    def create_val_set(x, y, ind_include):
+        return np.take(x, ind_include, axis=0), np.take(y, ind_include, axis=0)
+
+    # currently: no shuffling. we detect the non-empty labeled frames and split them. The validation set is
+    # entirely constituted of the frames corresponding to the split labeled frames, the training set is all the
+    # remaining images. The training gt is then the remaining labeled frames and empty frames.
+    
+    # get indices of labeled frames
+    n_labels = int(0.5 + perc_labels * gt.data.shape[0])
+    ind = zero_sum(gt)
+    assert n_labels < len(ind)
+
+    # split labeled frames between train and val sets
+    ind_train = np.random.choice(ind, size=n_labels, replace=False).tolist()
+    ind_val = list_diff(ind, ind_train)
+    assert len(ind_train) + len(ind_val) == len(ind)
+
+    # create train and val sets
+    x_train, y_train = create_train_set(data, gt, ind_val)
+    x_val, y_val = create_val_set(data, gt, ind_val)
+
+    # add channel dim and one-hot encoding
+    X = x_train[..., np.newaxis]
+    Y = convert_to_oneHot(y_train)
+    X_val = x_val[..., np.newaxis]
+    Y_val = convert_to_oneHot(y_val)
+
+    return X, Y, X_val, Y_val
 
 
 def __train():
