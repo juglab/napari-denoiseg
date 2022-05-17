ecfff7a02b4eaede4a39149228c1217689db09c8
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-05-17 11:42:05 +0200

    add sample data

--- src/napari_denoiseg/__init__.py
+++ src/napari_denoiseg/__init__.py
@@ -1,4 +1,5 @@
 __version__ = "0.0.1"
 
+from ._sample_data import denoiseg_data_n0
 from ._train_widget import TrainWidget
 from ._predict_widget import PredictWidget
--- src/napari_denoiseg/_sample_data.py
+++ src/napari_denoiseg/_sample_data.py
@@ -6,12 +6,32 @@ Replace code below according to your needs.
 """
 from __future__ import annotations
 
-from napari.types import LayerDataTuple, LabelsData
+import os
+import urllib
+import zipfile
+import numpy as np
+
+from napari.types import LayerDataTuple
 
 
-def denoiseg_sample() -> LayerDataTuple:
-    return None
+def denoiseg_data_n0() -> LayerDataTuple:
+    # create a folder for the data
+    if not os.path.isdir('./data'):
+        os.mkdir('data')
 
+    noise_level = 'n0'
+    link = 'https://zenodo.org/record/5156969/files/DSB2018_n0.zip?download=1'
 
-def denoiseg_labels() -> LabelsData:
-    return None
+    # check if data has been downloaded already
+    zipPath = "data/DSB2018_{}.zip".format(noise_level)
+    if not os.path.exists(zipPath):
+        # download and unzip data
+        data = urllib.request.urlretrieve(link, zipPath)
+        with zipfile.ZipFile(zipPath, 'r') as zip_ref:
+            zip_ref.extractall("data")
+
+    train_data = np.load('data/DSB2018_{}/train/train_data.npz'.format(noise_level))
+    images = train_data['X_train'].astype(np.float32)
+    labels = train_data['Y_train'].astype(np.int32)
+
+    return [(images, {'name': 'Data'}), (labels, {"name": 'Labels'})]
