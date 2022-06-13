d62bee61d912d2b5898d028e4052a91bccca21a2
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-06-13 15:20:16 +0200

    add threshold widget base

---
+++ src/napari_denoiseg/_threshold_widget.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+from qtpy.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QPushButton,
+    QProgressBar,
+    QSpinBox,
+    QFormLayout,
+    QComboBox,
+    QFileDialog,
+    QLabel,
+    QTabWidget
+)
+
+
+class ThresholdWiget(QWidget):
+    pass
