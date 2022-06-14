79885a851a815777be0dfb16a5562018410e8fd7
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-06-14 14:04:28 +0200

    remove batch predict

--- src/napari_denoiseg/__init__.py
+++ src/napari_denoiseg/__init__.py
@@ -3,5 +3,4 @@ __version__ = "0.0.1"
 from ._sample_data import denoiseg_data_n0, denoiseg_data_n10, denoiseg_data_n20
 from ._train_widget import TrainWidget
 from ._predict_widget import PredictWidget
-from ._batch_predict_widget import BatchPredictWidget
 from ._threshold_widget import ThresholdWiget
