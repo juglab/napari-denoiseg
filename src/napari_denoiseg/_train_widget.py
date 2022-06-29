865ee01c4e318251b584423352d76e3d8cf41f83
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-06-29 15:20:47 +0200

    update axes based on checkbox

--- src/napari_denoiseg/_train_widget.py
+++ src/napari_denoiseg/_train_widget.py
@@ -112,43 +112,30 @@ class TrainWidget(QWidget):
         self.patch_size_spin.setSingleStep(8)
         self.patch_size_spin.setValue(16)
 
+        # 3D checkbox
+        self.enable_3d = enable_3d()
+        self.patch_size_spin3D = QSpinBox()
+        self.patch_size_spin3D.setMaximum(512)
+        self.patch_size_spin3D.setMinimum(16)
+        self.patch_size_spin3D.setSingleStep(8)
+        self.patch_size_spin3D.setValue(16)
+        self.patch_size_spin3D.setVisible(False)
+        self.patch_size_spin3D_label = QLabel()
+        self.patch_size_spin3D_label.setText("Patch Z")
+        self.patch_size_spin3D_label.setVisible(False)
+
         # TODO add tooltips
         others = QWidget()
         formLayout = QFormLayout()
         formLayout.addRow('', self.axes_widget)
+        formLayout.addRow('Enable 3D', self.enable_3d.native)
         formLayout.addRow('N epochs', self.n_epochs_spin)
         formLayout.addRow('N steps', self.n_steps_spin)
         formLayout.addRow('Batch size', self.batch_size_spin)
         formLayout.addRow('Patch XY', self.patch_size_spin)
+        formLayout.addRow(self.patch_size_spin3D_label, self.patch_size_spin3D)
         others.setLayout(formLayout)
         self.layout().addWidget(others)
-
-        # load 3D enabling checkbox
-        self.enable_3d = enable_3d()
-        #self.layout().addWidget(self.enable_3d.native)
-        formLayout.addRow('Enable 3D', self.enable_3d.native)
-
-        # patch size 3D
-        def switch_visibility(state):
-            if state:
-                self.patch_size_spin3D.setVisible(True)
-                self.patch_size_spin3D_label.setVisible(True)
-            else:
-                self.patch_size_spin3D.setVisible(False)
-                self.patch_size_spin3D_label.setVisible(False)
-
-        self.patch_size_spin3D = QSpinBox()
-        self.patch_size_spin3D.setMaximum(512)
-        self.patch_size_spin3D.setMinimum(2)
-        self.patch_size_spin3D.setSingleStep(2)
-        self.patch_size_spin3D.setValue(2)
-        self.patch_size_spin3D.setVisible(False)
-        self.enable_3d.use3d.changed.connect(switch_visibility)
-        self.patch_size_spin3D_label = QLabel()
-        self.patch_size_spin3D_label.setText("Patch Z")
-        self.patch_size_spin3D_label.setVisible(False)
-        #self.layout().addWidget(self.patch_size_spin3D)
-        formLayout.addRow(self.patch_size_spin3D_label, self.patch_size_spin3D)
 
         # progress bars
         progress_widget = QWidget()
@@ -209,7 +196,6 @@ class TrainWidget(QWidget):
         self.plot = TBPlotWidget(max_width=300, max_height=300)
         self.layout().addWidget(self.plot.native)
 
-
         # place-holder for models and parameters (e.g. bioimage.io)
         self.worker = None
         self.model, self.threshold = None, None
@@ -218,6 +204,8 @@ class TrainWidget(QWidget):
         self.load_from_disk = False
 
         # actions
+        self.tabs.currentChanged.connect(self._update_tab_axes)
+        self.enable_3d.use3d.changed.connect(self._update_3D)
         self.images.changed.connect(self._update_layer_axes)
         self.train_images_folder.text_field.textChanged.connect(self._update_disk_axes)
         self.train_button.clicked.connect(lambda: self._start_training(self.model))
@@ -230,6 +218,12 @@ class TrainWidget(QWidget):
         self._update_layer_axes()
 
     def _start_training(self, pretrained_model=None):
+        """
+        Start training from scratch or using a pretrained model.
+
+        :param pretrained_model:
+        :return:
+        """
         if self.state == State.IDLE:
             # TODO check that all in order before predicting (data loaded, axes valid ...etc...)
 
@@ -257,6 +251,11 @@ class TrainWidget(QWidget):
                 pass
 
     def _done(self):
+        """
+        Called when training is finished.
+
+        :return:
+        """
         self.state = State.IDLE
         self.train_button.setText('Train new')
         self.zero_model_button.setEnabled(True)
@@ -267,14 +266,32 @@ class TrainWidget(QWidget):
         self.save_button.setEnabled(True)
 
     def _zero_model(self):
+        """
+        Zero the model, causing the next training session to train from scratch.
+        :return:
+        """
         if self.state == State.IDLE:
             self.model = None
 
-    def _update_3D(self):
-        # TODO: get checkbox state and pass it on to the axes
-        pass
+    def _update_3D(self, event):
+        """
+        Update the UI based on the status of the 3D checkbox.
+        :param event:
+        :return:
+        """
+        is_3D = event.value
+        self.patch_size_spin3D.setVisible(is_3D)
+        self.patch_size_spin3D_label.setVisible(is_3D)
+
+        # update axes widget
+        self.axes_widget.update_is_3D(is_3D)
+        self.axes_widget.set_text_field(self.axes_widget.get_default_text())
 
     def _update_layer_axes(self):
+        """
+        Update the axes widget based on the shape of the data selected in the layer selection drop-down widget.
+        :return:
+        """
         if self.images.value is not None:
             shape = self.images.value.data.shape
 
@@ -282,38 +299,70 @@ class TrainWidget(QWidget):
             self.axes_widget.update_axes_number(len(shape))
             self.axes_widget.set_text_field(self.axes_widget.get_default_text())
 
-    def _add_image(self, image):
-        if SAMPLE in self.viewer.layers:
-            self.viewer.layers.remove(SAMPLE)
-
-        if image is not None:
-            self.viewer.add_image(image, name=SAMPLE, visible=True)
-
-            # update the axes widget
-            self.axes_widget.update_axes_number(len(image.shape))
-            self.axes_widget.set_text_field(self.axes_widget.get_default_text())
-
     def _update_disk_axes(self):
+        """
+        Load an example image from the disk and update the axes widget based on its shape.
+
+        :return:
+        """
+        def add_image(widget, image):
+            if image is not None:
+                if SAMPLE in widget.viewer.layers:
+                    widget.viewer.layers.remove(SAMPLE)
+
+                widget.viewer.add_image(image, name=SAMPLE, visible=True)
+
+                # update the axes widget
+                widget.axes_widget.update_axes_number(len(image.shape))
+                widget.axes_widget.set_text_field(widget.axes_widget.get_default_text())
+
         path = self.train_images_folder.get_folder()
 
-        # load one image
-        load_worker = loading_worker(path)
-        load_worker.yielded.connect(lambda x: self._add_image(x))
-        load_worker.start()
+        if path is not None or path != '':
+            # load one image
+            load_worker = loading_worker(path)
+            load_worker.yielded.connect(lambda x: add_image(self, x))
+            load_worker.start()
+
+    def _update_tab_axes(self):
+        """
+        Updates the axes widget following the newly selected tab.
+
+        :return:
+        """
+        self.load_from_disk = self.tabs.currentIndex() == 1
+
+        if self.load_from_disk:
+            self._update_disk_axes()
+        else:
+            self._update_layer_axes()
 
     def _update_epochs(self):
+        """
+        Update the epoch progress bar following a change of total number of epochs.
+        :return:
+        """
         if self.state == State.IDLE:
             self.n_epochs = self.n_epochs_spin.value()
             self.pb_epochs.setValue(0)
             self.pb_epochs.setFormat(f'Epoch ?/{self.n_epochs_spin.value()}')
 
     def _update_steps(self):
+        """
+        Update the step progress bar following a change of total number of steps.
+        :return:
+        """
         if self.state == State.IDLE:
             self.n_steps = self.n_steps_spin.value()
             self.pb_steps.setValue(0)
             self.pb_steps.setFormat(f'Step ?/{self.n_steps_spin.value()}')
 
     def _update_all(self, updates):
+        """
+        Update the UI following an update event from the training worker.
+        :param updates:
+        :return:
+        """
         if self.state == State.RUNNING:
             if UpdateType.EPOCH in updates:
                 val = updates[UpdateType.EPOCH]
@@ -331,6 +380,10 @@ class TrainWidget(QWidget):
                 self.plot.update_plot(*updates[UpdateType.LOSS])
 
     def _save_model(self):
+        """
+        Export the model.
+        :return:
+        """
         if self.state == State.IDLE:
             if self.model:
                 where = QFileDialog.getSaveFileName(caption='Save model')[0]
