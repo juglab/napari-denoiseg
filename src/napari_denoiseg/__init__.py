98f4360eece7273f7a761f62e05e58d2935dd95f
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-06-30 18:04:29 +0200

    add 3D example data

--- src/napari_denoiseg/__init__.py
+++ src/napari_denoiseg/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.1"
 
-from ._sample_data import denoiseg_data_n0, denoiseg_data_n10, denoiseg_data_n20
+from ._sample_data import denoiseg_data_2D_n0, denoiseg_data_2D_n10, denoiseg_data_2D_n20, denoiseg_data_3D
 from ._train_widget import TrainWidget
 from ._predict_widget import PredictWidget
 from ._patch_widget import patch_creation
--- src/napari_denoiseg/_sample_data.py
+++ src/napari_denoiseg/_sample_data.py
@@ -10,7 +10,7 @@ import numpy as np
 from napari.types import LayerDataTuple
 
 
-def _download_data(noise_level):
+def _download_data_2D(noise_level):
     assert noise_level in ['n0', 'n10', 'n20']
 
     if noise_level == 'n0':
@@ -29,7 +29,7 @@ def _download_data(noise_level):
             zip_ref.extractall("data")
 
 
-def _load_data(noise_level):
+def _load_data_2D(noise_level):
     train_data = np.load('data/DSB2018_{}/train/train_data.npz'.format(noise_level))
     images = train_data['X_train'].astype(np.float32)
     labels = train_data['Y_train'].astype(np.int32)
@@ -37,23 +37,47 @@ def _load_data(noise_level):
     return [(images, {'name': 'Data'}), (labels, {"name": 'Labels'})]
 
 
-def _denoiseg_data(noise_level):
+def _denoiseg_data_2D(noise_level):
     # create a folder for the data
     if not os.path.isdir('./data'):
         os.mkdir('data')
 
-    _download_data(noise_level)
+    _download_data_2D(noise_level)
 
-    return _load_data(noise_level)
-
-
-def denoiseg_data_n0() -> LayerDataTuple:
-    return _denoiseg_data('n0')
+    return _load_data_2D(noise_level)
 
 
-def denoiseg_data_n10() -> LayerDataTuple:
-    return _denoiseg_data('n10')
+def _denoiseg_data_3D():
+    # create a folder for our data
+    if not os.path.isdir('./data'):
+        os.mkdir('data')
+
+    link = 'https://github.com/juglab/EmbedSeg/releases/download/v0.1.0/Mouse-Organoid-Cells-CBG.zip'
+
+    # check if data has been downloaded already
+    zipPath = 'data/Mouse-Organoid-Cells-CBG.zip'
+
+    # download the zip archive
+    if not os.path.exists(zipPath):
+        urllib.request.urlretrieve(link, zipPath)
+
+    # unzip the files
+    if not os.path.exists(zipPath[:-4]):
+        with zipfile.ZipFile(zipPath, 'r') as zip_ref:
+            zip_ref.extractall('data')
 
 
-def denoiseg_data_n20() -> LayerDataTuple:
-    return _denoiseg_data('n20')
+def denoiseg_data_2D_n0() -> LayerDataTuple:
+    return _denoiseg_data_2D('n0')
+
+
+def denoiseg_data_2D_n10() -> LayerDataTuple:
+    return _denoiseg_data_2D('n10')
+
+
+def denoiseg_data_2D_n20() -> LayerDataTuple:
+    return _denoiseg_data_2D('n20')
+
+
+def denoiseg_data_3D() -> LayerDataTuple:
+    return _denoiseg_data_3D()
