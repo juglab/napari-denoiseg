3569103ae51250131fcde24c45bc1a253763e5bb
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-08-24 16:58:14 +0200

    fix wrong import

--- src/napari_denoiseg/utils/__init__.py
+++ src/napari_denoiseg/utils/__init__.py
@@ -29,4 +29,3 @@ from .optimizer_worker import optimizer_worker
 from .prediction_worker import prediction_worker
 from .training_worker import training_worker
 from .loading_worker import loading_worker
-from .text import *
