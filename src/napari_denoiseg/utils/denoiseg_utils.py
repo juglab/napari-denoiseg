7fa575e2e84ac780fd3389c7d0b2ece00a2d6743
Tom Burke <burke@mpi-cbg.de>
2022-09-08 10:51:40 +0200

    Remove config.json despite error thrown

--- src/napari_denoiseg/utils/denoiseg_utils.py
+++ src/napari_denoiseg/utils/denoiseg_utils.py
@@ -80,9 +80,8 @@ def build_modelzoo(path, weights, inputs, outputs, tf_version, axes='byxc'):
                     tensorflow_version=tf_version
                     )
         head, _ = os.path.split(path)
-        head = os.path.join(head, "config.json").replace("/","\\")
-        print(head)
-        os.remove(head)
+        head = os.path.join(os.path.normcase(head), "config.json")
+        os.remove(os.path.abspath(head))
 
 
 def get_shape_order(shape_in, axes_in, ref_axes):
