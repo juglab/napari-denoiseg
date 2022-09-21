86694ec914164c194cdf4d0a17473141270edfdf
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-09-21 11:56:16 +0200

    fix resources for windows

---
+++ src/napari_denoiseg/_tests/test_resources.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from napari_denoiseg.resources import *
+
+
+def test_gear():
+    assert Path(ICON_GEAR).exists()
+    assert type(ICON_GEAR) == str
+
+
+def test_github():
+    assert Path(ICON_GITHUB).exists()
+    assert type(ICON_GITHUB) == str
+
+
+def test_juglab():
+    assert Path(ICON_JUGLAB).exists()
+    assert type(ICON_JUGLAB) == str
+
+
+def test_tf():
+    assert Path(ICON_TF).exists()
+    assert type(ICON_TF) == str
+
+
+def test_bioimage():
+    assert Path(DOC_BIOIMAGE).exists()
+    assert type(DOC_BIOIMAGE) == str
